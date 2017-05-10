6d88b62f2554cc0b1c0b7470d50f532702d27935
Grégoire Willmann <gregwillm@gmail.com>
2017-05-10 09:06:06 +0200

    Improved server and added README.md

---
+++ README.md
@@ -0,0 +1,38 @@
+# Digital Photo Frame Project
+
+## Made with <3 with a Raspberry Pi 3
+
+### Introduction
+
+I have made a digital photo frame as a side project.
+@See details [here](https://medium.com/@Gr3g0ire/un-cadre-photo-digital-19aee3bfddbc) (sorry in French for now) 
+To simplify the control of the slideshow, I have written a quick app with React Native.
+
+The app repo is =>  
+
+This repo contains the code for the raspberry written in Python
+
+### Installation
+
+Place server.py somewhere in your raspberry.
+Run it after every boot :
+
+`crontab -e`
+
+then add at the end : 
+```
+@boot python path_to_server.py
+```
+
+### License
+
+
+### Thanks
+This side project was greatly inspired by 
+
+- 
+- 
+
+
+Without thoses resources, it would probably have never seen the bright
+lights of Github repos!
--- server.py
+++ server.py
@@ -2,13 +2,15 @@ import socket
 import sys
 import os
 import json
+import glob
+import subprocess
 
-#PI_ADDRESS = '192.168.1.103'
-PI_ADDRESS = 'localhost'
+PI_ADDRESS = '192.168.1.103'
+#PI_ADDRESS = 'localhost'
 PI_PORT = 10000
 APP_PORT = 10001
-#SLIDESHOW_ROOT_FOLDER = '/mnt/wdmycloud/Shared Pictures/'
-SLIDESHOW_ROOT_FOLDER = '/Applications/'
+SLIDESHOW_ROOT_FOLDER = '/mnt/wdmycloud/Shared Pictures/'
+#SLIDESHOW_ROOT_FOLDER = '/Applications/'
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -19,7 +21,30 @@ print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
 # Save status of slideshow
-slideshowRunning = False 
+process = subprocess.Popen('ls', stdout=subprocess.PIPE)
+
+def is_running():
+    return process.poll() == None
+
+def launch_slideshow(pictures_path):
+    os.environ['DISPLAY'] = ':0.0'
+    os.environ['XAUTHORITY'] = '/home/pi/.Xauthority'
+
+    cmd = ['/usr/local/bin/feh', '--quiet', '--preload', '--auto-rotate', '-V', '-Z', '-F', '-Y', '-r', '-R 60', '-D 15.0', pictures_path]
+    return subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+# Remove folders with trailing dots
+def listdir_nohidden(path):
+    folders = []
+    for folder in glob.glob(path + '*'):
+        file_count = 0
+        for file in glob.glob(folder + '/*.*'):
+                file_count += 1
+        f = []
+        f.append(folder.split('/')[len(folder.split('/')) - 1])
+        f.append(file_count)
+        folders.append(f)
+    return sorted(folders, key=lambda x: x[0].lower)
 
 while True:
     print >>sys.stderr, '\nwaiting to receive message'
@@ -31,21 +56,23 @@ while True:
 
     action, param = data.split(';')
 
-    if slideshowRunning == False and action =="start":
-        slideshowRunning = True
-        #        os.system('DISPLAY=:0.0 XAUTHORITY=/home/pi/.Xauthority /usr/local/bin/feh --quiet --preload --auto-rotate -Z -F -Y -r -R 60 -D 15.0 "SLIDESHOW_ROOT_FOLDER"' + param + " &")
+    dataToSendBack = ''
+    if is_running() == False and action =="start":
+        process = launch_slideshow(SLIDESHOW_ROOT_FOLDER + param)
         print >>sys.stderr, "Diaporama en cours " + param
         dataToSendBack = 'status;running'
-        
-    if slideshowRunning == True and action =="stop":
-        slideshowRunning = False
+
+    if is_running() == True and action =="stop":
         print >>sys.stderr, "Diaporama ko"
-        os.system('pkill feh')
+        process.terminate()
         dataToSendBack = 'status;ko'
-    
+
     if action == "listfolders":
-        dataToSendBack = 'folders;' + json.dumps(os.listdir(SLIDESHOW_ROOT_FOLDER))
-                    
+        dataToSendBack = 'folders;' + json.dumps((listdir_nohidden(SLIDESHOW_ROOT_FOLDER)))
+
+    if action == "getstatus":
+        dataToSendBack = 'slideshowstatus;' + str(is_running())
+
     if dataToSendBack:
         sent = sock.sendto(dataToSendBack, (ip, APP_PORT))
-        print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
+        print >>sys.stderr, 'sent %s bytes back to %s :\n %s' % (sent, address, dataToSendBack)
